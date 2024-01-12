ddef5a6b0ec47e850ea22dbf7a90d01614e2139a
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-01-12 10:31:06 +0100

    nuevas calidades añadidas, hay que reconfigurar el quality.py

--- extras/config.py
+++ extras/config.py
@@ -1,54 +1,104 @@
 from pathlib import Path
 
 AUDIO_OPTS = {
-    'baja': {
+    
+    '96kbps': {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '96',  # Baja calidad
         }],
+        'writethumbnail': True,
     },
-    'media': {
+    
+    '128kbps': {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '128',  # Calidad media
         }],
+        'writethumbnail': True,
     },
-    'alta': {
+    
+    '192kbps': {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',  # Alta calidad
         }],
+        'writethumbnail': True,
+    },
+    
+    '256kbps': {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '256kbps',  # Alta calidad
+        }],
+        'writethumbnail': True,
+    },
+    
+    '320kbps': {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',  # Alta calidad
+        }],
+        'writethumbnail': True,
     },
 }
 
 
 VIDEO_OPTS = {
-    'alta': {
-        'format': 'bestvideo+bestaudio/best',  # Descarga la mejor calidad de video y audio
+    
+    '480p': {
+        'format': 'bestvideo[height<=480]+bestaudio',  # Descarga la mejor calidad de video y audio
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',  # Convertir a mp4 si es necesario
         }],
+        'writethumbnail': True,
     },
-    'media': {
-        'format': 'bestvideo+bestaudio/best',  # Descarga la mejor calidad de video y audio
+    
+    '720p': {
+        'format': 'bestvideo[height<=720]+bestaudio',  # Descarga la mejor calidad de video y audio
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',  # Convertir a mp4 si es necesario
         }],
+        'writethumbnail': True,
     },
-    'baja': {
-        'format': 'bestvideo+bestaudio/best',  # Descarga la mejor calidad de video y audio
+    
+    '1080p': {
+        'format': 'bestvideo[height<=1080]+bestaudio',  # Descarga la mejor calidad de video y audio
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',  # Convertir a mp4 si es necesario
         }],
+        'writethumbnail': True,
+    },
+    
+    '1440p': {
+        'format': 'bestvideo[height<=1440]+bestaudio',  # Descarga la mejor calidad de video y audio
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',  # Convertir a mp4 si es necesario
+        }],
+        'writethumbnail': True,
+    },
+    
+    '4k': {
+        'format': 'bestvideo[height<=2160]+bestaudio',  # Descarga la mejor calidad de video y audio
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',  # Convertir a mp4 si es necesario
+        }],
+        'writethumbnail': True,
     },
 }
 
